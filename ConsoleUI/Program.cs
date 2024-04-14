36b36ae0a89a4fd4fdb60a4db6cd18e155417722
Aytunc <aytuncalptekin@gmail.com>
2024-04-14 03:35:09 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductTest();
+
+//CategoryTest();
+
+static void ProductTest()
+{
+    ProductManager productManager = new ProductManager(new EfProductDal());
+    foreach (var item in productManager.GetProductDetails())
+    {
+        Console.WriteLine(item.ProductName + "/" + item.CategoryName);
+    }
+}
+
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
+}
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    //generic constraint : kısıtlar 
+    //class referas tip
+    //IEntity IEntity olabilir veya IEntity 
+    //new(): newlenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new() //generic, referans tip ve sadece IEntity dahil olanları eklemesi için
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);      
+       
+    }
+}
